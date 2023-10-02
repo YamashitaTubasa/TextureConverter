b534f4a21389e530c029815ddb967f56b0260b9c
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-10-02 15:08:31 +0900

    02_03

--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,10 +1,15 @@
-
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	printf("Hellow,World!\n");
+	// argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		// •¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		// ‰üs
+		printf("\n");
+	}
 
 	system("pause");
 
