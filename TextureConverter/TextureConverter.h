1e73f60e6ceecea8527faabd601c42be5c9bb43f
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-10-23 12:12:50 +0900

    02_04

---
+++ TextureConverter/TextureConverter.h
@@ -0,0 +1,35 @@
+/**
+ * @file TextureConverter
+ * @brief テクスチャコンバーター
+ * @author Yamashita Tubasa
+ */
+
+#pragma once
+
+#include <string>
+#include <windows.h>
+
+class TextureConverter
+{
+public:
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConvertTextureWICToDDS(const std::string& filePath);
+
+private:
+	/// <summary>
+	/// テクスチャファイルの読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+};
+
--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,15 +1,42 @@
+#include "TextureConverter.h"
+
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include <DirectXTex.h>
+
+// コマンドライン引数
+enum Argument {
+	kApplicationPath, // アプリケーションパス
+	kFilePath,        // 渡されたファイルパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[]) {
 
 	// argcの数だけ繰り返す
-	for (int i = 0; i < argc; i++) {
-		// 文字列argvのi番を表示
-		printf(argv[i]);
-		// 改行
-		printf("\n");
-	}
+	//for (int i = 0; i < argc; i++) {
+	//	// 文字列argvのi番を表示
+	//	printf(argv[i]);
+	//	// 改行
+	//	printf("\n");
+	//}
+
+	assert(argc >= NumArgument);
+
+	// COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバーター
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 
